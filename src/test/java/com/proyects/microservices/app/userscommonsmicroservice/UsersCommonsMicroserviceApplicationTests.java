8e8eb82e60a298a1d2be1a20a112d94e7b835bd9
pabloesperi <pabloesperi@hotmail.com>
2020-10-11 12:23:06 +0200

    second commit

---
+++ src/test/java/com/proyects/microservices/app/userscommonsmicroservice/UsersCommonsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyects.microservices.app.userscommonsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersCommonsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
